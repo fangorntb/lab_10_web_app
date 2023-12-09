82b39094b86e53a4ed944d7ebd3481493a8a6850
superuser <cyrillbelyakov@vk.com>
2023-12-09 12:28:25 +0700

    test app

--- gateway/gateway/settings.py
+++ gateway/gateway/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,18 +24,31 @@ SECRET_KEY = 'django-insecure-a$n8ot-o_hbk$lx_dv+ht6(@@fbbk=1#11hb2nq(57)iw(u9)!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = (
+    '192.168.0.141 '
+    '10.212.66.10 '
+    '172.17.0.1 '
+    '172.22.0.1 '
+    '172.18.0.1 '
+    '172.21.0.1 '
+    '172.19.0.1 '
+    '10.8.0.209 '
+    '127.0.0.1 '
+    'localhost'
+).split()
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "vacancies",
+    "drf_yasg",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -69,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gateway.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -79,7 +90,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- gateway/vacancies/views.py
+++ gateway/vacancies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+
+class Hello(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    @swagger_auto_schema(operation_id='get-hello', )
+    def get(self, request: Request):
+        return Response(data=f"hello, Django", status=status.HTTP_200_OK)
